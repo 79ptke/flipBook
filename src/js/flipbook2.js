086a2b8f063657d52d5616cf0b3c2938d3a19b64
79ptke <82435745+79ptke@users.noreply.github.com>
2023-03-05 23:19:20 +0900

    20230305_flipbook prev,next 버튼 구현중

--- src/js/flipbook2.js
+++ src/js/flipbook2.js
@@ -10,12 +10,15 @@ window.addEventListener('load', function() {
             // back 생성
             const $back = document.createElement('div');
             $back.classList.add('back');
+            $back.classList.add('card');
             // prev 버튼 생성
             const $prev = document.createElement('div');
             $prev.classList.add('prev');
+            $prev.classList.add('btn');
             // next 버튼 생성
             const $next = document.createElement('div');
             $next.classList.add('next');
+            $next.classList.add('btn');
 
             $pages.innerHTML = front.outerHTML;
             front.parentNode.insertBefore($pages, front); // front 페이지 pages로 감싸기
@@ -26,6 +29,31 @@ window.addEventListener('load', function() {
         });
         // 첫번째 페이지에 current 클래스 추가
         document.querySelector('.pages').classList.add('current');
+        
+        //prev 버튼 클릭
+        document.addEventListener('click',function(e){
+            if(e.target && e.target.className === 'prev btn'){
+                  if(document.querySelector('.pages').classList.contains('current')) {
+                      alert('첫 페이지 입니다.');
+                  } else {
+                      document.querySelector('.current').classList.remove('current');
+
+                  }
+             }
+         });
+         //next 버튼 클릭
+        document.addEventListener('click',function(e){
+            if(e.target && e.target.className === 'next btn'){
+                document.querySelector('.current').classList.remove('flip_right');
+                document.querySelector('.current').classList.add('flip_left');
+                //   if(document.querySelector('.pages').classList.contains('current')) {
+                //       alert('첫 페이지 입니다.');
+                //   } else {
+                //       document.querySelector('.current').classList.remove('current');
+
+                //   }
+             }
+         });
     }
 
     paging();
