1f963ae2ccd6972284954d4cebb7e233d76ba568
79ptke <82435745+79ptke@users.noreply.github.com>
2023-03-12 22:45:33 +0900

    20230312_flipbook 반응형 구현중
    pc -> mobile 완료(메뉴 및 북마크 수정해야함)
    <수정사항>
    - mobile -> pc 수정
    - resize 시 계속 div 생성 되는것 수정
    - 겹치는 코드가 많음

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 ### jquery로 구현한 html 반응형 구현중
 
 - javascript로 바꾸기 (jquery 사용하지 말자)
+- pc -> mobile 완료(메뉴 및 북마크 수정해야함)
+- mobile -> pc 수정
+- resize 시 계속 div 생성 되는것 수정
+- 겹치는 코드가 많음 (나중에 함수 만들어서 수정)
--- src/js/flipbook.js
+++ src/js/flipbook.js
@@ -208,24 +208,51 @@ $(document).ready(function() {
 $(window).on('resize', function(){
     console.log('resize event!');
     if (window.innerWidth >= 768) {
+        // pc 사이즈 
       if($("#flipBook").hasClass("pc")) {
-        //$(".back").remove();
-        //wrapper();
+        $(".back").remove();
+        $(".card:nth-child(2n)").addClass("back");
+        // page 앞 뒷면 wrap
+        var divs = $(".wrapper > div");
+        for (let i = 0; i < divs.length; i += 2) {
+            divs.slice(i, i + 2).wrapAll("<section class='pages'></section>");
+        }
+        // 첫번째 페이지에 current 클래스 추가
+        $(".pages:first-child").addClass("current");
+        // next, prev 버튼 추가
+        $(".pages").append(`
+            <div class="btn prev" alt="이전버튼"></div>
+            <div class="btn next" alt="다음버튼"></div>
+        `);
+
       } else {
         $("#flipBook").addClass("pc").removeClass("mobile");
       }
     } 
     else {
+        // 모바일 사이즈
         $("#flipBook").removeClass("pc").addClass("mobile"); 
         $(".front").removeClass("back");
-        if($(".pages .back").length === 0) {
+        if($(".pages .back").length < 1) {
+            $(".front").unwrap(".pages"); // 기존에 있는 pages 없애기
+            $(".btn").remove(); // 버튼 삭제
+            $(".front").wrap("<section class='pages'></section>");
             $(".front").after("<div class='back card'></div>");
+            // 첫번째 페이지에 current 클래스 추가
+            $(".pages:first-child").addClass("current");
+            // next, prev 버튼 추가
+            $(".pages").append(`
+                <div class="btn prev" alt="이전버튼"></div>
+                <div class="btn next" alt="다음버튼"></div>
+            `);
+            // id추가
+            $(".pages").each(function(index) {
+                $(this).attr('id', 'p'+index);
+                // nav 안 li 생성
+                $("nav ul").append("<li data-page='p" +index+"'>"+$(this).find('.front').attr('title')+"</li>")
+            });
         }
-        // page 앞 뒷면 wrap
-        // var divs = $(".wrapper .front");
-        // for (let i = 0; i < divs.length; i += 2) {
-        //     divs.slice(i, i + 2).wrapAll("<section class='pages'></section>");
-        // }
+        
         //wrapper();
     }
 });
